d125cd57097d340c15f1bd4e64ce0084b95a093e
Pedro <pedro.amp01@yahoo.com>
2024-04-05 16:39:26 -0300

    Versão Básica

--- instrucoes.h
+++ instrucoes.h
@@ -11,22 +11,13 @@ struct TabelaInstrucoes {
     char *funct7[8];
 };
 
-struct Instrucoes {
-    char opcode[20];
-    char rd[5];
-    char rs1[5];
-    char rs2[5];
-    char imm[20];
-};
+
 
 int obterInstrucao(struct TabelaInstrucoes *tabela, char *nomeInstrucao, char *regDestino, char *regFonte1, char *regFonte2, FILE *arquivoSaida);
 void registrador_para_binario(char *reg, char *binario);
 void inteiro_para_binario(int numero, char *binario, int bits);
 void imediato_para_binario(char *imediato, char *binario);
 void escrever_no_arquivo(FILE *arquivo, const char *codigo_maquina);
-
-//Parte de teste
-struct Instrucoes analisar_instrucao(char instrucao[]);
 
 
 #endif /* INSTRUCOES_H */
