f5aaaf737390e6fa765c248f0fe6f39bd055d908
CoderAugusto <137956951+CoderAugusto@users.noreply.github.com>
2024-04-19 13:43:18 -0300

    Organização final
    Organização final e colocando as instruções na entrada

--- entrada.asm
+++ entrada.asm
@@ -1,20 +1,10 @@
-lb x1, 0(x2)
-lh x3, 4(x4)
-lw x5, 8(x6)
-addi x7, x8, 100
-andi x9, x10, 200
-ori x11, x12, 300
-sb x13, 12(x14)
-sh x15, 16(x16)
-sw x17, 20(x18)
-add x19, x20, x21
-sub x22, x23, x24
-and x25, x26, x27
-or x28, x29, x30
-xor x31, x32, x33
-sll x34, x35, 4
-srl x36, x37, 2
-bne x38, x39, 1024
-beq x40, x41, 2048
-mv x42, x43
-lui x15, 240
+add x2, x0, x1
+sll x1, x2, x2
+or x2, x2, x1
+andi x2, x1, 16
+addi x3, x2, -243
+add x2, x0, x1
+add x6, x0, x2
+sub x3, x6, x2
+xor x4, x2, x3
+srl x0, x2, x2
